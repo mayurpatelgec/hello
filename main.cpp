72aea84de8c33683fd365649467b5ec5b392d1bc
Mayur Patel <mayurpatel.gec@gmail.com>
2018-09-13 16:18:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 ///Returns the value of x multiplied by 2,
 /// except for 42, which is multiplied by one
 int do_magic(const int x) {
-if (x == 42) {
-return 42;
-}
-return x ∗ 2;
+  if (x == 42) {
+    return 42;
+  }
+  return (x∗2);
 }
 int main() {
-std : : cout << do_magic(2) << '\n ' ;
+std::cout << do_magic(2) << '\n ' ;
 //Forgot to test do_magic(42)
 }
